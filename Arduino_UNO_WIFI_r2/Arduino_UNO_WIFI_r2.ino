afd00aa4fb0e211f97aaaadddcf94bdb7c70c1b2
smnmsr <40615828+smnmsr@users.noreply.github.com>
2024-09-20 12:26:31 +0200

    Update Arduino_UNO_WIFI_r2.ino

--- Arduino_UNO_WIFI_r2/Arduino_UNO_WIFI_r2.ino
+++ Arduino_UNO_WIFI_r2/Arduino_UNO_WIFI_r2.ino
@@ -1,11 +1,11 @@
+#include <SPI.h>
 #include <WiFiNINA.h>
 #include <HCSR04.h>
 #include "secrets.h"
 
 // Define variables
-const int TRIGGER_PIN = 2;   // Pin D2, Ultrasonic sensor
-const int ECHO_PIN = 5;      // Pin D5, Ultrasonic sensor
-const int LED = LED_BUILTIN; // Indicator LED
+const int TRIGGER_PIN = 2; // Pin D2, Ultrasonic sensor
+const int ECHO_PIN = 5;    // Pin D5, Ultrasonic sensor
 
 const int MEASURE_CORRECTION = 9; // Added to measure value since te tabletop is higher that the sensor
 
@@ -13,13 +13,17 @@ const int MEASURE_CORRECTION = 9; // Added to measure value since te tabletop is
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
+// measurement buffer
+const uint8_t BUFFER_SIZE = 10;
+static uint8_t buffer_index = 0;
+static uint8_t loop_index = 0;
+static uint8_t loop_mod = 10;
+static double buffer[BUFFER_SIZE];
+
 void setup(void)
 {
-  // Enable Serial for testing purposes
-  Serial.begin(115200);
-
-  // Setup LED
-  pinMode(LED, OUTPUT);
+  // Enable Serial for DEBUG purposes
+  // Serial.begin(115200);
 
   // Setup ultrasonic sensor
   HCSR04.begin(TRIGGER_PIN, ECHO_PIN);
@@ -36,8 +40,8 @@ void setup(void)
   WiFi.setHostname(HOSTNAME);
 
   // attempt to connect to WiFi network
-  Serial.print("Attempting to connect to Network named: ");
-  Serial.println(WIFI_SSID); // print the network name (SSID);
+  // Serial.print("Attempting to connect to Network named: ");
+  // Serial.println(WIFI_SSID); // print the network name (SSID);
   while (status != WL_CONNECTED)
   {
     // Connect to WPA/WPA2 network.
@@ -48,12 +52,18 @@ void setup(void)
   }
 
   // Print debug info via serial
-  Serial.println();
-  Serial.print("Connected - IP: ");
-  Serial.println(WiFi.localIP());
+  // Serial.println();
+  // Serial.print("Connected - IP: ");
+  // Serial.println(WiFi.localIP());
 
   // start the server
   server.begin();
+
+  // init buffer
+  for (uint8_t i = 0; i < BUFFER_SIZE; i++)
+  {
+    buffer[i] = 0;
+  }
 }
 
 void loop()
@@ -63,6 +73,21 @@ void loop()
   {
     // reset arduino by caling reset function at address 0x00
     asm volatile("jmp 0x00"); // reset
+  }
+
+  // new measurement in buffer
+  if (loop_index >= loop_mod)
+  {
+    // measure distance and store in buffer
+    buffer[buffer_index] = *HCSR04.measureDistanceCm();
+
+    // check if measurement is valid (GREATERTHAN 0)
+    if (buffer[buffer_index] > 0)
+    {
+      buffer_index = (buffer_index + 1) % BUFFER_SIZE;
+    }
+
+    loop_index = 0;
   }
 
   WiFiClient client = server.available(); // listen for incoming clients
@@ -76,7 +101,6 @@ void loop()
       if (client.available())
       { // if there's bytes to read from the client,
 
-        digitalWrite(LED, HIGH);
         char c = client.read(); // read a byte
         if (c == '\n')
         { // if the byte is a newline character
@@ -86,9 +110,15 @@ void loop()
           if (currentLine.length() == 0)
           {
             // measure the height
-            double *height = HCSR04.measureDistanceCm();
+            double height = 0;
+            for (uint8_t i = 0; i < BUFFER_SIZE; i++)
+            {
+              height += buffer[i];
+            }
+            height = round(height / BUFFER_SIZE);
+
             char response[50];
-            sprintf(response, "{ \"table_height\": %d }", (int)(*height) + MEASURE_CORRECTION);
+            sprintf(response, "{ \"table_height\": %d }", (int)height + MEASURE_CORRECTION);
 
             // send height to client
             client.println("HTTP/1.1 200 OK");
@@ -114,11 +144,12 @@ void loop()
     // close the connection:
     client.stop();
     // Serial.println("client disconnected");
-    digitalWrite(LED, LOW);
   }
   else
   {
     // sleep one second
     delay(1000);
   }
+
+  loop_index++;
 }
